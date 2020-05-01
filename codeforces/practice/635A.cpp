47c38361b2d733c242904ba12ef1db29e8756a0b
nishant141077 <nishant141077@gmail.com>
2020-05-01 16:30:03 +0530

    practice questions CF

---
+++ codeforces/practice/635A.cpp
@@ -0,0 +1,33 @@
+/*
+    Author : Nishant Gupta 2.0
+*/
+#include<bits/stdc++.h>
+
+#define LL long long int
+#define getcx getchar_unlocked
+#define X first
+#define Y second
+#define PB push_back
+#define MP make_pair
+#define MAX 100005
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define INFL (LL(1e18))
+#define rep(i, a, n) for(i=a;i<n;i++)
+#define rev(i, a, n) for(i=a;i>=n;i--)
+#define MSV(A,a) memset(A, a, sizeof(A))
+#define rep_itr(itr, c) for(itr = (c).begin(); itr != (c).end(); itr++)
+
+using namespace std;
+
+int main() {
+	int a, b, c, d;
+	int t;
+	cin>>t;
+	while(t--) {
+		cin>>a>>b>>c>>d;
+		cout<<b<<" "<<c<<" "<<c<<endl;
+	}
+    return 0;
+}
+
