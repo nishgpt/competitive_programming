ff490bdd7c823f5e859c3ba39b6af8ede44218e4
nishant141077 <nishant141077@gmail.com>
2020-05-23 12:31:15 +0530

    added 615D CF solution

---
+++ codeforces/practice/615D.cpp
@@ -0,0 +1,51 @@
+/*
+    Author : Nishant Gupta 2.0
+*/
+#include<bits/stdc++.h>
+
+#define LL long long int
+#define getcx getchar_unlocked
+#define X first
+#define Y second
+#define PB push_back
+#define MP make_pair
+#define MAX 100005
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define INFL (LL(1e18))
+#define chk(a) cerr << endl << #a << " : " << a << endl
+#define chk2(a,b) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << endl
+#define chk3(a,b,c) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << endl
+#define chk4(a,b,c,d) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << "\t" << #d << " : " << d << endl
+#define rep(i, a, n) for(i=a;i<n;i++)
+#define rev(i, a, n) for(i=a;i>=n;i--)
+#define MSV(A,a) memset(A, a, sizeof(A))
+#define rep_itr(itr, c) for(itr = (c).begin(); itr != (c).end(); itr++)
+
+using namespace std;
+
+int main() {
+	int q, x, y, i;
+	
+	scanf("%d %d", &q, &x);
+	int a[x];
+	for(i=0;i<x;i++) a[i] = i;
+	
+	map<int, bool> present;
+	int np = 0;
+	while(q--) {
+		scanf("%d", &y);
+		int mod = y % x;
+		present[a[mod]] = true;
+		a[mod] += x;
+		
+		while(true) {
+			if(!present[np]) break;
+			++np;
+		}
+		printf("%d\n", np); 
+	}
+
+    return 0;
+}
+
