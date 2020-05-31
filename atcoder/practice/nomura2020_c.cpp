745cfb8ab28a411d5320ff4b6097b6c9be87de1d
nishant141077 <nishant141077@gmail.com>
2020-05-31 17:03:41 +0530

    nomura 2020 C problem soltuion

---
+++ atcoder/practice/nomura2020_c.cpp
@@ -0,0 +1,71 @@
+/*
+    Author : Nishant Gupta 2.0
+*/
+#include<bits/stdc++.h>
+
+#define LL long long int
+#define getcx getchar_unlocked
+#define X first
+#define Y second
+#define PB push_back
+#define MP make_pair
+#define MAX 100005
+#define LOG_MAX 20
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define INFL (LL(1e18))
+#define chk(a) cerr << endl << #a << " : " << a << endl
+#define chk2(a,b) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << endl
+#define chk3(a,b,c) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << endl
+#define chk4(a,b,c,d) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << "\t" << #d << " : " << d << endl
+#define rep(i, a, n) for(i=a;i<n;i++)
+#define rev(i, a, n) for(i=a;i>=n;i--)
+#define MSV(A,a) memset(A, a, sizeof(A))
+#define rep_itr(itr, c) for(itr = (c).begin(); itr != (c).end(); itr++)
+
+using namespace std;
+
+inline void inp( int &n ) { 
+	n=0;    int ch=getcx();int sign=1;   
+	while( ch < '0' || ch > '9' ){if(ch=='-')sign=-1; ch=getcx();} 
+	while(  ch >= '0' && ch <= '9' )    
+	n = (n<<3)+(n<<1) + ch-'0', ch=getcx();
+	n=n*sign;
+}
+
+inline void inp1( LL &n ) { 
+	n=0;    LL ch=getcx();LL sign=1;   
+	while( ch < '0' || ch > '9' ){if(ch=='-')sign=-1; ch=getcx();} 
+	while(  ch >= '0' && ch <= '9' )    
+	n = (n<<3)+(n<<1) + ch-'0', ch=getcx();
+	n=n*sign;
+}
+
+int main() {
+	int n, i;
+	cin>>n;
+	
+	LL a[n+1], suml = 0;
+	for(i=0;i<=n;i++) {
+		cin>>a[i];
+		suml += a[i];
+	}
+	
+	LL nodes = 1, ans = 0;
+	for(i=0;i<=n;i++) {
+		ans += nodes;
+		if(nodes < a[i]) {
+			cout<<-1<<endl;
+			return 0;
+		}
+		
+		LL non_leaf = nodes - a[i];
+		suml -= a[i];
+		
+		nodes = min(2*non_leaf, suml);
+	}
+
+	cout<<ans<<endl;
+    return 0;
+}
+
