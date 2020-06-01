266775ebe14407877f161e19d2f57e94ad979f55
nishant141077 <nishant141077@gmail.com>
2020-06-02 00:43:35 +0530

    practice solutions of atcoder contests

---
+++ atcoder/practice/abc169_f.cpp
@@ -0,0 +1,77 @@
+/*
+    Author : Nishant Gupta 2.0
+*/
+#include<bits/stdc++.h>
+
+#define LL long long int
+#define getcx getchar_unlocked
+#define X first
+#define Y second
+#define PB push_back
+#define MP make_pair
+#define MAX 3005
+#define LOG_MAX 20
+#define MOD 998244353
+#define INF 0x3f3f3f3f
+#define INFL (LL(1e18))
+#define chk(a) cerr << endl << #a << " : " << a << endl
+#define chk2(a,b) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << endl
+#define chk3(a,b,c) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << endl
+#define chk4(a,b,c,d) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << "\t" << #d << " : " << d << endl
+#define rep(i, a, n) for(i=a;i<n;i++)
+#define rev(i, a, n) for(i=a;i>=n;i--)
+#define MSV(A,a) memset(A, a, sizeof(A))
+#define rep_itr(itr, c) for(itr = (c).begin(); itr != (c).end(); itr++)
+
+using namespace std;
+
+inline void inp( int &n ) { 
+	n=0;    int ch=getcx();int sign=1;   
+	while( ch < '0' || ch > '9' ){if(ch=='-')sign=-1; ch=getcx();} 
+	while(  ch >= '0' && ch <= '9' )    
+	n = (n<<3)+(n<<1) + ch-'0', ch=getcx();
+	n=n*sign;
+}
+
+inline void inp1( LL &n ) { 
+	n=0;    LL ch=getcx();LL sign=1;   
+	while( ch < '0' || ch > '9' ){if(ch=='-')sign=-1; ch=getcx();} 
+	while(  ch >= '0' && ch <= '9' )    
+	n = (n<<3)+(n<<1) + ch-'0', ch=getcx();
+	n=n*sign;
+}
+
+int a[MAX];
+int dp[MAX][MAX];
+
+/* Modulo exponentiation */
+LL power(LL base, LL exp, LL ans) {
+	if(!exp) return ans % MOD;
+	if(exp&1) ans = (ans * base) % MOD;
+	return power((base * base) % MOD, exp>>1, ans);
+}
+
+int solve(int pos, int sum, int n) {
+	if(sum < 0) return 0;
+		
+	if(sum == 0) return power(2, n-pos, 1);
+	
+	if(pos == n) return 0;
+	
+	if(dp[pos][sum] != -1) return dp[pos][sum];
+	
+	dp[pos][sum] = ((2 * solve(pos+1, sum, n)) % MOD + solve(pos+1, sum-a[pos], n)) % MOD;
+	
+	return dp[pos][sum];
+}
+
+int main() {
+	int n, s;	
+	cin>>n>>s;
+	MSV(dp, -1);
+	for(int i=0;i<n;i++) cin>>a[i];
+	
+	cout<<solve(0, s, n)<<endl;
+    return 0;
+}
+
