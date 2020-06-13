1cf4c1c5f2cdeb7e2e5a60b561dedceb237da24d
nishant141077 <nishant141077@gmail.com>
2020-06-13 17:25:22 +0530

    solutions to 563 div 2 practice (A and B only)

---
+++ codeforces/practice/563A.cpp
@@ -0,0 +1,58 @@
+/*
+    Author : Nishant Gupta 2.0
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define LL long long int
+#define getcx getchar_unlocked
+#define X first
+#define Y second
+#define PB push_back
+#define MP make_pair
+#define MAX 100005
+#define LOG_MAX 20
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define INFL (LL(1e18))
+#define chk(a) cerr << endl << #a << " : " << a << endl
+#define chk2(a,b) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << endl
+#define chk3(a,b,c) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << endl
+#define chk4(a,b,c,d) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << "\t" << #d << " : " << d << endl
+#define rep(i, a, n) for(i=a;i<n;i++)
+#define rev(i, a, n) for(i=a;i>=n;i--)
+#define in(x) scanf("%d", &x)
+#define inl(x) scanf("%lld", &x)
+#define in2(x, y) scanf("%d %d", &x, &y)
+#define inl2(x, y) scanf("%lld %lld", &x, &y)
+#define MSV(A,a) memset(A, a, sizeof(A))
+#define rep_itr(itr, c) for(itr = (c).begin(); itr != (c).end(); itr++)
+typedef pair<int, int> pi;
+typedef pair<LL, LL> pl;
+const char en = '\n';
+
+void solve() {
+	int n;
+	cin>>n;
+	int a[2*n];
+	for(int i=0;i<2*n;i++) in(a[i]);
+	sort(a, a+2*n);
+	
+	LL sum = 0, sum1 = 0;
+	for(int i=0;i<n;i++) sum+= a[i];
+	for(int i=n;i<2*n;i++) sum1+=a[i];
+	
+	if(sum == sum1) cout<<"-1";
+	else for(int i=0;i<2*n;i++) cout<<a[i]<<" ";
+	cout<<en;
+}
+
+int main() {
+	int t = 1;
+	
+	//~cin>>t;
+	while(t--) {
+		solve();
+	}
+    return 0;
+}
