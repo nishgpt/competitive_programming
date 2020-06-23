715d2eb5476afa53cbaa17cb32526d7ea215597c
nishant141077 <nishant141077@gmail.com>
2020-06-23 23:26:04 +0530

    CF 652 div2 solutions

---
+++ codeforces/652_div2/D.cpp
@@ -0,0 +1,72 @@
+/*
+    Author : Nishant Gupta 2.0
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define LL long long int
+#define getcx getchar_unlocked
+#define X first
+#define Y second
+#define PB push_back
+#define MP make_pair
+#define MAX 2000005
+#define LOG_MAX 20
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define INFL (LL(1e18))
+#define chk(a) cerr << endl << #a << " : " << a << endl
+#define chk2(a,b) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << endl
+#define chk3(a,b,c) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << endl
+#define chk4(a,b,c,d) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << "\t" << #d << " : " << d << endl
+#define rep(i, a, n) for(i=a;i<n;i++)
+#define rev(i, a, n) for(i=a;i>=n;i--)
+#define in(x) scanf("%d", &x)
+#define inl(x) scanf("%lld", &x)
+#define in2(x, y) scanf("%d %d", &x, &y)
+#define inl2(x, y) scanf("%lld %lld", &x, &y)
+#define MSV(A,a) memset(A, a, sizeof(A))
+#define rep_itr(itr, c) for(itr = (c).begin(); itr != (c).end(); itr++)
+#define finish(x) {cout<<x<<'\n'; return;}
+typedef pair<int, int> pi;
+typedef pair<LL, LL> pl;
+const char en = '\n';
+
+
+/* Modulo exponentiation */
+LL power(LL base, LL exp, LL ans) {
+	if(!exp) return ans % MOD;
+	if(exp&1) ans = (ans * base) % MOD;
+	return power((base * base) % MOD, exp>>1, ans);
+}
+
+LL ans[MAX];
+void pre() {
+	ans[0] = 0;
+	ans[1] = 0;
+	ans[2] = 0;
+	ans[3] = 4;
+	ans[4] = 4;
+	
+	for(int i=5;i<MAX;i++) {
+		ans[i] = ((ans[i-1] + (2 * ans[i-2]) % MOD) % MOD) % MOD;
+		if(i % 3==0) ans[i] = (ans[i] + 4) % MOD;
+	}
+}
+
+void solve() {
+	LL n;
+	inl(n);
+	
+	cout<<ans[n]<<en;
+}
+
+int main() {
+	int t = 1;
+	pre();
+	cin>>t;
+	while(t--) {
+		solve();
+	}
+    return 0;
+}
