f9bd38ff1a345aa3049490cd847d8d1ad40dbdee
nishant141077 <nishant141077@gmail.com>
2020-07-09 01:14:07 +0530

    practice solutions

---
+++ atcoder/practice/abc173_E.cpp
@@ -0,0 +1,59 @@
+/*
+    Author : Nishant Gupta 2.0
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define LL long long int
+#define getcx getchar_unlocked
+#define X first
+#define Y second
+#define PB push_back
+#define MP make_pair
+#define MAX 100005
+#define LOG_MAX 20
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define INFL (LL(1e18))
+#define chk(a) cerr << endl << #a << " : " << a << endl
+#define chk2(a,b) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << endl
+#define chk3(a,b,c) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << endl
+#define chk4(a,b,c,d) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << "\t" << #d << " : " << d << endl
+#define rep(i, a, n) for(i=a;i<n;i++)
+#define rev(i, a, n) for(i=a;i>=n;i--)
+#define in(x) scanf("%d", &x)
+#define inl(x) scanf("%lld", &x)
+#define in2(x, y) scanf("%d %d", &x, &y)
+#define inl2(x, y) scanf("%lld %lld", &x, &y)
+#define MSV(A,a) memset(A, a, sizeof(A))
+#define rep_itr(itr, c) for(itr = (c).begin(); itr != (c).end(); itr++)
+#define finish(x) {cout<<x<<'\n'; return;}
+typedef pair<int, int> pi;
+typedef pair<LL, LL> pl;
+const char en = '\n';
+
+void solve() {
+	int n;
+	in(n);
+	LL ec = 0, vc = 0;
+	vector<bool> vis(n+1, false);
+	for(int i=0;i<n-1;i++) {
+		int x, y;
+		in2(x, y);
+		if(!vis[x]) vc += 1L * x * (n-x+1);
+		if(!vis[y]) vc += 1L * y * (n-y+1);
+		vis[x] = true;vis[y] = true;
+		ec += (1L * min(x, y) * (n - max(x, y)+1));
+	}
+	cout<<(n == 1 ? 1 : vc-ec)<<en;
+}
+
+int main() {
+	int t = 1;
+	
+	//~cin>>t;
+	while(t--) {
+		solve();
+	}
+    return 0;
+}
