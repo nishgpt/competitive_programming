6f3b15c358482ffb73f13c82277d3308ffaf007f
nishant141077 <nishant141077@gmail.com>
2020-07-25 11:33:52 +0530

    CF 659 div2 solutions

---
+++ codeforces/659_div2/C.cpp
@@ -0,0 +1,77 @@
+/*
+    Author : Nishant Gupta 2.0
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define LL long long int
+#define getcx getchar_unlocked
+#define X first
+#define Y second
+#define PB push_back
+#define MP make_pair
+#define MAX 20
+#define LOG_MAX 20
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define INFL (LL(1e18))
+#define chk(a) cerr << endl << #a << " : " << a << endl
+#define chk2(a,b) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << endl
+#define chk3(a,b,c) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << endl
+#define chk4(a,b,c,d) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << "\t" << #d << " : " << d << endl
+#define rep(i, a, n) for(i=a;i<n;i++)
+#define rev(i, a, n) for(i=a;i>=n;i--)
+#define in(x) scanf("%d", &x)
+#define inl(x) scanf("%lld", &x)
+#define in2(x, y) scanf("%d %d", &x, &y)
+#define inl2(x, y) scanf("%lld %lld", &x, &y)
+#define MSV(A,a) memset(A, a, sizeof(A))
+#define rep_itr(itr, c) for(itr = (c).begin(); itr != (c).end(); itr++)
+#define finish(x) {cout<<x<<'\n'; return;}
+typedef pair<int, int> pi;
+typedef pair<LL, LL> pl;
+const char en = '\n';
+
+
+/* Union Find / DSU */
+//have a parent array global
+int parent[MAX];
+int find(int u) {
+	if(u == parent[u])return u;
+	return parent[u] = find(parent[u]);
+}
+
+void _union(int u, int v) {
+	parent[find(u)] = find(v);
+}
+
+void solve() {	
+	int n;in(n);
+	string s, t;
+	cin>>s>>t;
+	for(int i=0;i<MAX;i++) parent[i] = i;
+	
+	for(int i=0;i<n;i++) {
+		if(t[i] == s[i]) continue;
+		if(s[i] > t[i]) finish("-1");
+		_union(s[i]-'a', t[i]-'a'); 
+	}
+	
+	int freq[MAX] = {0};
+	for(int i=0;i<MAX;i++) freq[find(i)]++;
+	
+	int ans = 0;
+	for(int i=0;i<MAX;i++) ans += max(0, freq[i]-1);
+	
+	cout<<ans<<en;
+}
+
+int main() {
+	int t = 1;
+	
+	cin>>t;
+	while(t--) {
+		solve();
+	}
+    return 0;
+}
