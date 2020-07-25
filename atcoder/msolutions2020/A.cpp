679309439c4bdaa4f927ac4896c8fe1644d8dca6
nishant141077 <nishant141077@gmail.com>
2020-07-25 19:55:46 +0530

    atcoder solutions for M-solutions contest

---
+++ atcoder/msolutions2020/A.cpp
@@ -0,0 +1,58 @@
+/*
+    Author : Nishant Gupta 2.0
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define LL long long int
+#define getcx getchar_unlocked
+#define X first
+#define Y second
+#define PB push_back
+#define MP make_pair
+#define MAX 100005
+#define LOG_MAX 20
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define INFL (LL(1e18))
+#define chk(a) cerr << endl << #a << " : " << a << endl
+#define chk2(a,b) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << endl
+#define chk3(a,b,c) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << endl
+#define chk4(a,b,c,d) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << "\t" << #d << " : " << d << endl
+#define rep(i, a, n) for(i=a;i<n;i++)
+#define rev(i, a, n) for(i=a;i>=n;i--)
+#define in(x) scanf("%d", &x)
+#define inl(x) scanf("%lld", &x)
+#define in2(x, y) scanf("%d %d", &x, &y)
+#define inl2(x, y) scanf("%lld %lld", &x, &y)
+#define MSV(A,a) memset(A, a, sizeof(A))
+#define rep_itr(itr, c) for(itr = (c).begin(); itr != (c).end(); itr++)
+#define finish(x) {cout<<x<<'\n'; return;}
+typedef pair<int, int> pi;
+typedef pair<LL, LL> pl;
+const char en = '\n';
+
+void solve() {
+	int x; cin>>x;
+	int ans = -1;
+	if(x >= 400 && x < 600) ans = 8;
+	if(x >= 600 && x < 800) ans = 7;
+	if(x >= 800 && x < 1000) ans = 6;
+	if(x >= 1000 && x < 1200) ans = 5;
+	if(x >= 1200 && x < 1400) ans = 4;
+	if(x >= 1400 && x < 1600) ans = 3;
+	if(x >= 1600 && x < 1800) ans = 2;
+	if(x >= 1800 && x < 2000) ans = 1;
+	
+	cout<<ans<<en;
+}
+
+int main() {
+	int t = 1;
+	
+	//~cin>>t;
+	while(t--) {
+		solve();
+	}
+    return 0;
+}
