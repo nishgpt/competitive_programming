d5f869fb2c5c69de936ba655ab914c8e9e49f0f8
nishant141077 <nishant141077@gmail.com>
2020-08-01 20:58:17 +0530

    august easy 2020 practice

---
+++ hackerearth/august-easy-20/maximum-bitwise-pair-bb846052.cpp
@@ -0,0 +1,71 @@
+/*
+    Author : Nishant Gupta 2.0
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define LL long long int
+#define getcx getchar_unlocked
+#define X first
+#define Y second
+#define PB push_back
+#define MP make_pair
+#define MAX 131072
+#define LOG_MAX 18
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define INFL (LL(1e18))
+#define chk(a) cerr << endl << #a << " : " << a << endl
+#define chk2(a,b) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << endl
+#define chk3(a,b,c) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << endl
+#define chk4(a,b,c,d) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << "\t" << #d << " : " << d << endl
+#define rep(i, a, n) for(i=a;i<n;i++)
+#define rev(i, a, n) for(i=a;i>=n;i--)
+#define in(x) scanf("%d", &x)
+#define inl(x) scanf("%lld", &x)
+#define in2(x, y) scanf("%d %d", &x, &y)
+#define inl2(x, y) scanf("%lld %lld", &x, &y)
+#define MSV(A,a) memset(A, a, sizeof(A))
+#define rep_itr(itr, c) for(itr = (c).begin(); itr != (c).end(); itr++)
+#define finish(x) {cout<<x<<'\n'; return;}
+typedef pair<int, int> pi;
+typedef pair<LL, LL> pl;
+const char en = '\n';
+
+void solve() {
+	int n;
+	in(n);
+	
+	vector<int> a(n+1), dp(MAX);
+	dp[0] = -1;
+	for(int i=1;i<=n;i++) {
+		in(a[i]);
+		dp[i] = a[i];
+	}
+	for(int i=n+1;i<MAX;i++) dp[i] = -1;
+	
+	for(int bit=0;bit<LOG_MAX;bit++) {
+		for(int i=0;i<MAX;i++) {
+			if(i & (1<<bit)) dp[i] = max(dp[i], dp[i ^ (1<<bit)]);
+		}
+	}
+	
+	int q;
+	in(q);
+	while(q--) {
+		int idx; in(idx);
+		int ans = dp[idx ^ (MAX-1)];
+		if(ans != -1) ans += a[idx];
+		cout<<ans<<en;
+	}
+}
+
+int main() {
+	int t = 1;
+	
+	cin>>t;
+	while(t--) {
+		solve();
+	}
+    return 0;
+}
