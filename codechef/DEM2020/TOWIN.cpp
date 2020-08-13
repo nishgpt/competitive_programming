77272040b2999336f86f19f3df6c2080079a9fc5
nishant141077 <nishant141077@gmail.com>
2020-08-14 00:09:50 +0530

    dementia codechef 2020

---
+++ codechef/DEM2020/TOWIN.cpp
@@ -0,0 +1,67 @@
+/*
+    Author : Nishant Gupta 2.0
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define LL long long int
+#define getcx getchar_unlocked
+#define X first
+#define Y second
+#define PB push_back
+#define MP make_pair
+#define MAX 100005
+#define LOG_MAX 20
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define INFL (LL(1e18))
+#define chk(a) cerr << endl << #a << " : " << a << endl
+#define chk2(a,b) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << endl
+#define chk3(a,b,c) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << endl
+#define chk4(a,b,c,d) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << "\t" << #d << " : " << d << endl
+#define rep(i, a, n) for(i=a;i<n;i++)
+#define rev(i, a, n) for(i=a;i>=n;i--)
+#define in(x) scanf("%d", &x)
+#define inl(x) scanf("%lld", &x)
+#define in2(x, y) scanf("%d %d", &x, &y)
+#define inl2(x, y) scanf("%lld %lld", &x, &y)
+#define MSV(A,a) memset(A, a, sizeof(A))
+#define rep_itr(itr, c) for(itr = (c).begin(); itr != (c).end(); itr++)
+#define finish(x) {cout<<x<<'\n'; return;}
+typedef pair<int, int> pi;
+typedef pair<LL, LL> pl;
+const char en = '\n';
+
+void solve() {
+	int n;in(n);
+	int a[n];
+	for(int i=0;i<n;i++) in(a[i]);
+	
+	sort(a, a+n);
+	
+	LL s1 = 0, s2 = 0;
+	s1 += a[n-1];
+	if(n > 1) s2 += a[n-2];
+	if(n > 2) s2 += a[n-3];
+	
+	bool f = true;
+	for(int i=n-4;i>=0;i--) {
+		if(f) s1 += a[i];
+		else s2 += a[i];
+		f = !f;
+	}
+	
+	if(s1 > s2) cout<<"first"<<en;
+	else if(s1 < s2) cout<<"second"<<en;
+	else cout<<"draw"<<en;
+}
+
+int main() {
+	int t = 1;
+	
+	cin>>t;
+	while(t--) {
+		solve();
+	}
+    return 0;
+}
