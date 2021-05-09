7f46ffc17caaaf3dd665887c405d82edeb628d30
nishant141077 <nishant141077@gmail.com>
2021-05-09 16:05:41 +0530

    atcoder practice solutions

---
+++ atcoder/practice/zone2021_b.cpp
@@ -0,0 +1,79 @@
+/*
+    Author : Nishant Gupta 2.0
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define LL long long int
+#define getcx getchar_unlocked
+#define X first
+#define Y second
+#define PB push_back
+#define MP make_pair
+#define MAX 100005
+#define LOG_MAX 20
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define INFL (LL(1e18))
+#define chk(a) cerr << endl << #a << " : " << a << endl
+#define chk2(a,b) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << endl
+#define chk3(a,b,c) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << endl
+#define chk4(a,b,c,d) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << "\t" << #d << " : " << d << endl
+#define rep(i, a, n) for(i=a;i<n;i++)
+#define rev(i, a, n) for(i=a;i>=n;i--)
+#define in(x) scanf("%d", &x)
+#define inl(x) scanf("%lld", &x)
+#define in2(x, y) scanf("%d %d", &x, &y)
+#define inl2(x, y) scanf("%lld %lld", &x, &y)
+#define MSV(A,a) memset(A, a, sizeof(A))
+#define rep_itr(itr, c) for(itr = (c).begin(); itr != (c).end(); itr++)
+#define finish(x) {cout<<x<<'\n'; return;}
+typedef pair<int, int> pi;
+typedef pair<LL, LL> pl;
+const char en = '\n';
+
+void solve() {
+	int n, d, h;
+	in(n);in2(d, h);
+	
+	vector<pair<double, double> > v;
+	for(int i=0;i<n;i++) {
+		double x, y;
+		cin>>x>>y;
+		v.PB(MP(x, y));
+	}
+	
+	int low = 0, high = 10000000;
+	double ans = -1;
+	
+	double x2 = d, y2 = h;
+	
+	while(low<=high) {
+		int mid = (low + high) >> 1;
+		double y1 = (mid * 1.0) / 10000.0;
+		//~chk(y1);
+		
+		bool possible = true;
+		for(int i=0;i<n;i++) { 
+			double y = (((y2 - y1) * v[i].X) / x2) + y1;
+			if(y < v[i].Y) {possible =false;break;}
+		}
+		
+		if(possible) {
+			ans = y1;
+			high = mid-1;
+		} else low = mid + 1;
+	}
+	
+	printf("%.6lf\n", ans);
+}
+
+int main() {
+	int t = 1;
+	
+	//~cin>>t;
+	while(t--) {
+		solve();
+	}
+    return 0;
+}
