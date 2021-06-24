00b3fd130c9a8971ba3bcf4567eb6f199f6f91a1
nishant141077 <nishant141077@gmail.com>
2021-06-24 20:26:57 +0530

    practice solution codeforces

---
+++ codeforces/practice/725D.cpp
@@ -0,0 +1,74 @@
+/*
+	Author : Nishant Gupta 2.0
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define LL long long int
+#define getcx getchar_unlocked
+#define X first
+#define Y second
+#define PB push_back
+#define MP make_pair
+#define MAX 100005
+#define LOG_MAX 20
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define INFL (LL(1e18))
+#define chk(a) cerr << endl << #a << " : " << a << endl
+#define chk2(a,b) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << endl
+#define chk3(a,b,c) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << endl
+#define chk4(a,b,c,d) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << "\t" << #d << " : " << d << endl
+#define rep(i, a, n) for(i=a;i<n;i++)
+#define rev(i, a, n) for(i=a;i>=n;i--)
+#define in(x) scanf("%d", &x)
+#define inl(x) scanf("%lld", &x)
+#define in2(x, y) scanf("%d %d", &x, &y)
+#define inl2(x, y) scanf("%lld %lld", &x, &y)
+#define MSV(A,a) memset(A, a, sizeof(A))
+#define rep_itr(itr, c) for(itr = (c).begin(); itr != (c).end(); itr++)
+#define finish(x) {cout<<x<<'\n'; return;}
+typedef pair<int, int> pi;
+typedef pair<LL, LL> pl;
+const char en = '\n';
+
+int get_ops(int num) {
+	int cnt = 0, cp = num;
+	for (int i = 2; i * i <= num; i++) {
+		while (cp % i == 0) {
+			cp /= i;
+			cnt++;
+		}
+	}
+	if (cp > 1) cnt++;
+	return cnt;
+}
+
+void solve() {
+	int a, b, k;
+	in2(a, b); in(k);
+
+	if (k == 1) {
+		if ((a % b == 0 || b % a == 0) && a != b) finish("YES");
+		finish("NO");
+	}
+
+	int max_ops = get_ops(a) + get_ops(b);
+	if (k <= max_ops) finish("YES");
+	finish("NO");
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
