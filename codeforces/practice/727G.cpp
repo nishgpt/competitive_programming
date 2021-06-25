7a7fcbfd873afaff9866fbddbbad88d1f28d243d
nishant141077 <nishant141077@gmail.com>
2021-06-25 18:19:03 +0530

    practice solution codeforces

---
+++ codeforces/practice/727G.cpp
@@ -0,0 +1,75 @@
+/*
+	Author : Nishant Gupta 2.0
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define LL long long int
+#define getcx getchar_unlocked
+#define X first
+#define Y second
+#define PB push_back
+#define MP make_pair
+#define MAX 1000000007
+#define LOG_MAX 20
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define INFL (LL(1e18))
+#define chk(a) cerr << endl << #a << " : " << a << endl
+#define chk2(a,b) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << endl
+#define chk3(a,b,c) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << endl
+#define chk4(a,b,c,d) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << "\t" << #d << " : " << d << endl
+#define rep(i, a, n) for(i=a;i<n;i++)
+#define rev(i, a, n) for(i=a;i>=n;i--)
+#define in(x) scanf("%d", &x)
+#define inl(x) scanf("%lld", &x)
+#define in2(x, y) scanf("%d %d", &x, &y)
+#define inl2(x, y) scanf("%lld %lld", &x, &y)
+#define MSV(A,a) memset(A, a, sizeof(A))
+#define rep_itr(itr, c) for(itr = (c).begin(); itr != (c).end(); itr++)
+#define finish(x) {cout<<x<<'\n'; return;}
+typedef pair<int, int> pi;
+typedef pair<LL, LL> pl;
+const char en = '\n';
+
+void solve() {
+	LL x, y, a, b;
+	inl2(x, y);
+	inl2(a, b);
+
+	if (a == b) {
+		finish(min(x, y) / a);
+	}
+
+	LL low = 0, high = (x + y) / (a + b), ans = 0;
+	if (a < b) swap(a, b);
+
+	while (low <= high) {
+		LL mid = (low + high) >> 1;
+
+		LL l = ceil((mid * a - y * 1.0) / (a - b));
+		LL r = (x - mid * b) / (a - b);
+
+		if (r >= l && r >= 0 && x >= mid * b && y >= mid * b) {
+			ans = mid;
+			low = mid + 1;
+		} else high = mid - 1;
+	}
+
+	cout << ans << en;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
