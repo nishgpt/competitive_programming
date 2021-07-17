b706f69913e6126f4cbfd997a1432d144435753b
nishant141077 <nishant141077@gmail.com>
2021-07-17 19:56:28 +0530

    atcoder abc210 solutions

---
+++ atcoder/abc210/C.cpp
@@ -0,0 +1,75 @@
+/*
+	Author : Nishant Gupta 2.0
+*/
+#include<bits/stdc++.h>
+#include<unordered_map>
+using namespace std;
+
+#define LL long long int
+#define getcx getchar_unlocked
+#define X first
+#define Y second
+#define PB push_back
+#define MP make_pair
+#define MAX 100005
+#define LOG_MAX 20
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define INFL (LL(1e18))
+#define chk(a) cerr << endl << #a << " : " << a << endl
+#define chk2(a,b) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << endl
+#define chk3(a,b,c) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << endl
+#define chk4(a,b,c,d) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << "\t" << #d << " : " << d << endl
+#define rep(i, a, n) for(i=a;i<n;i++)
+#define rev(i, a, n) for(i=a;i>=n;i--)
+#define in(x) scanf("%d", &x)
+#define inl(x) scanf("%lld", &x)
+#define in2(x, y) scanf("%d %d", &x, &y)
+#define inl2(x, y) scanf("%lld %lld", &x, &y)
+#define MSV(A,a) memset(A, a, sizeof(A))
+#define rep_itr(itr, c) for(itr = (c).begin(); itr != (c).end(); itr++)
+#define finish(x) {cout<<x<<'\n'; return;}
+typedef pair<int, int> pi;
+typedef pair<LL, LL> pl;
+const char en = '\n';
+
+void solve() {
+	int n, k; in2(n, k);
+
+	vector<int> v(n);
+	int i, j;
+	rep(i, 0, n) {
+		in(v[i]);
+	}
+
+	unordered_map<int, int> mp;
+	rep(i, 0, k) {
+		mp[v[i]]++;
+	}
+
+	int maxi = mp.size();
+	rep(i, k, n) {
+		mp[v[i - k]]--;
+		if (mp[v[i - k]] == 0) mp.erase(v[i - k]);
+		mp[v[i]]++;
+		int sz = mp.size();
+		maxi = max(maxi, sz);
+	}
+
+	cout << maxi << en;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+
+	// cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
